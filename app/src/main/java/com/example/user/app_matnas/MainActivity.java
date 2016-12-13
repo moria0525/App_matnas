d303227a251fff90384625bda4bbf12870dfd326
moria0525 <moria0525@gmail.com>
2016-12-13 19:17:47 +0200

    screen:home

---
+++ app/src/main/java/com/example/user/app_matnas/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.app_matnas;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
