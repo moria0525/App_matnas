ae86979e21c052ad951fd87dc2328581eb9e179c
moria0525 <moria0525@gmail.com>
2016-12-13 23:51:59 +0200

    add screen ContactUs

--- app/src/main/java/com/example/user/app_matnas/MainActivity.java
+++ app/src/main/java/com/example/user/app_matnas/MainActivity.java
@@ -4,16 +4,34 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ImageButton contactus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        contactus =(ImageButton)findViewById(R.id.contactus);
 
     }
+    public void onClickContactUsIcon(View view)
+    {
+        Intent i = new Intent(this,activity_contactUs.class);
+        startActivity(i);
+    }
 }
+
+
+
---
+++ app/src/main/java/com/example/user/app_matnas/activity_contactUs.java
@@ -0,0 +1,18 @@
+package com.example.user.app_matnas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class activity_contactUs extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+
+    }
+}
